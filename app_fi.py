cb2a16bb97cf5c42f378a1c79e97c97c62d82f66
ushasrivanama <114283901+ushasrivanama@users.noreply.github.com>
2024-03-26 10:28:44 +0530

    Update app_fi.py

--- app_fi.py
+++ app_fi.py
@@ -249,4 +249,4 @@ if uploaded_file is not None:
     	st.write(f'**{text}** is a registered plate number')
     
     else:
-    	st.write(f'<style="color:blue;">{text} is not a registered number',unsafe_allow_html=True)
+    	st.write(f'<p style="color:blue;">{text}</p> is not a registered number',unsafe_allow_html=True)
